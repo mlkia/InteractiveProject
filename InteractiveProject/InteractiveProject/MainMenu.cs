74173b4786016531a24b6d0c8fcd687d13e4eb42
Josefin Persson <josefinp94@gmail.com>
2022-02-23 10:36:05 +0100

    jobbar på menyn

---
+++ InteractiveProject/InteractiveProject/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using Interactive_Saga;
+
+public class MainMenu
+{
+    MessageHelper messageHelper = new();
+
+    public void RunMenu()
+    {
+        while (/* user.Level < 4 */ true) // så länge man inte hunnit vinna över alla 3 bossar
+        {
+            messageHelper.MainMenuMessage();
+
+            string userMenuInput = Console.ReadLine();
+
+            int menuInputInt = 0;
+            int.TryParse(userMenuInput, out menuInputInt);
+
+            switch (menuInputInt)
+            {
+                case 1:
+                    Console.Clear();
+                    // anropa RunGame();
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    // user.ShowStats();
+                    break;
+
+                case 3:
+                    Console.Clear();
+                    messageHelper.RulesOfGame();
+                    break;
+
+                case 4:
+                    // user.Level = 4;
+                    break;
+
+                default:
+                    messageHelper.MenuErrorMessage();
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
---
+++ InteractiveProject/InteractiveProject/MessageHelper.cs
@@ -0,0 +1,173 @@
+﻿namespace Interactive_Saga
+{
+    public class MessageHelper
+    {
+        public void WelcomeMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("      *  ' *    ''  **            **                                  '* * *      * ''  ");
+            Console.WriteLine("* *  ' *    ''        ***   ''****                  **             '*   *   ' *    ''*  ");
+            Console.WriteLine("*     *  '      ''  **                ''  **         *   *      ''    '* * *      *   * ");
+            Console.WriteLine("* *  '      ''  **          ''**                                            *     *     ");
+            Console.WriteLine("  *  ' *    ''    ''****         **     ''   '*** *                 '    '***  *        ");
+            Console.WriteLine("      *  ' *    ''  **        **                     **            '* * *      * ''     ");
+            Console.WriteLine("  *  ' *    ''  **    ***            '                             * '***  *    ''   *  ");
+            Console.WriteLine("*        ' *    ''  **                ''*  *         **      ''    '    *      *      * ");
+            Console.WriteLine("  *  ' *    ''  **          ''*  *                                   **     ''          ");
+            Console.WriteLine("*        ' *    ''  **        **      ''*  *         **            '* *    *      * ''  ");
+            Console.WriteLine("  *  ' *    ''  *                *  *'                     *        *         *    ''*  ");
+            Console.WriteLine(" *         *              *    **                    *                            *     ");
+            Console.WriteLine("      *          * *                       *                                *           ");
+            Thread.Sleep(2000);
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("      *  ' *    ''  **            **                                  '* * *      * ''  ");
+            Console.WriteLine("* *  ' *    ''        ***   ''****                  **             '  * *   ' *    ''*  ");
+            Console.WriteLine("*     *  '      ''  **                ''  **         *   *      ''    '* * *      *   * ");
+            Console.WriteLine("    ''  **          ''**                                   *        *             *     ");
+            Console.WriteLine("  *  ' *    ''    ''****         **     ''   '*** *                 '    '     *        ");
+            Console.WriteLine("*     *  ' *    ''  **        **                     **            '*   *      * ''     ");
+            Console.WriteLine("' *    ''  **    ***            '                             * '*     **  *    ''   *  ");
+            Console.WriteLine("    *  ' *    ''  **                ''*  *         **      ''    '    *      *        * ");
+            Console.WriteLine("  *  ' *    ''  **          ''*  *                                    *     ''          ");
+            Console.WriteLine("   ''  **        **      ''*  *         **            '*             **    *      * ''  ");
+            Console.WriteLine("*        ' *    ''  **                ''             **      ''                   *   * ");
+            Console.WriteLine("   ''              ''****         **     ''   '*** *       '  * '*         **     *     ");
+            Console.WriteLine("*     *  ' *    ''  **        **      ''** *         **            '  * *      * ''     ");
+            Console.WriteLine("* *  ' *    ''  **    ***   ''             **                        '  *     *    ''*  ");
+            Console.WriteLine("   ''  **                ''             **      ''    '**             * *         *   * ");
+            Console.WriteLine("* *  ' *    '  ***          ''****         **     ''   '*** *       '    '***  *        ");
+            Console.WriteLine("*     *  ' *    ''  **        **      ''****         **            '    *      * ''     ");
+            Console.WriteLine("     ' *    ''  **    ***   ''*  *         **                              *    ''*     ");
+            Console.WriteLine("*        ' *    ''  **                ''  **         **      ''    '*** *         *   * ");
+            Console.WriteLine("     ' *    ''  **          ''****         **     ''   '    *       ' ** '        *     ");
+            Console.WriteLine("     ''  *                *  *'                     *        *         *    ''*         ");
+            Console.WriteLine("        *              *    **                    *                            *        ");
+            Console.WriteLine("      *          * *                       *                                *           ");
+            Thread.Sleep(2000);
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("*     *  ' *    ''  **        **      ''                **            '    *      * ''  ");
+            Console.WriteLine("    ''  **    ***   ''**     **                   '                   *            ''*  ");
+            Console.WriteLine("*     *  ' *    ''  **                ''  **         *   *      ''    '  * *      *   * ");
+            Console.WriteLine("   ''  **          ''  **       **       ''   '*   ** *       '             '***  *     ");
+            Console.WriteLine("*     *  ' *    ''  **        **      ''*  *         **               '*** *      * ''  ");
+            Console.WriteLine("    ''  **    ***   ''*  *   *                 '*** *              '  *       *    ''*  ");
+            Console.WriteLine("*     *  ' *    ''  **                ''*  *         **      ''       '    *      *   * ");
+            Console.WriteLine("    ''  **          ''              *     ''                 *                    *     ");
+            Console.WriteLine("*     *  ' *    ''  **        **      ''             **            '       *      * ''  ");
+            Console.WriteLine("* *  ' *    ''  **    ***   ''*  *         **          '                      *    ''*  ");
+            Console.WriteLine("     *  ' *    ''  **                ''****         **       ''    '    *         *   * ");
+            Console.WriteLine("  *  ' *    ''  **          ''             **     ''   '*** *       '    '***  *        ");
+            Console.WriteLine("      *  ' *    ''  **        **      ''   *         **            '    *      * ''     ");
+            Console.WriteLine("  ' *    ''  **    ***   '   '        **                     *                *    ''*  ");
+            Console.WriteLine("     *  ' *    ''  **                 ''*  *         **      ''    '*** *         *   * ");
+            Console.WriteLine(" *  '      ''  **           ''****         **     ''   '  * *       '    '***  *        ");
+            Console.WriteLine("*     *  ' *    ''  **        **  ''****             **            '* * *      * ''     ");
+            Console.WriteLine(" *  '      ''        ** *   ''                **       '                        ''   *  ");
+            Console.WriteLine("     *  ' *    ''  **                 ''             **      ''    '* * *      *      * ");
+            Console.WriteLine("                            ''   *         **     ''   '*** *       '    '***  *        ");
+            Console.WriteLine("     *  ' *    ''   **        **      ''             **            '* * *      * ''     ");
+            Console.WriteLine("  *  ' *    ''  **    ***   ''*  *         **          '  * *   '    '* *  *    ''*     ");
+            Console.WriteLine("     *  ' *    ''   **                ''****         **      ''    '*   *         *   * ");
+            Console.WriteLine(" *  ' *    ''  * *          ''*  *         **     ''   '*** *       '    '* *     *     ");
+            Console.WriteLine("     *  ' *     ''  **        **      ''**    **         **         '    *     * ''     ");
+            Console.WriteLine("  ' *    ''     **    ***   ''*  *         **          '             '  *     *    ''*  ");
+            Console.WriteLine("*     *  ' *    ''  **                ''             **      ''    '*** *         *   * ");
+            Console.WriteLine("  ' *       '  ***          ''**           **     ''   '  * *       '    '*    *        ");
+            Console.WriteLine("*     *  ' *    ''  **        **      ''             **            '*   *      * ''     ");
+            Console.WriteLine("* *                         ''             **          '         *              ''   *  ");
+            Console.WriteLine("*     *  ' *    ''  **                ''*  *         **      ''    '    *      *      * ");
+            Console.WriteLine("  ' *       ''  **          ''*  *         **     ''   '*   *       '    '***  *        ");
+            Console.WriteLine("      *  ' *    ''  **        **      ''*  *         **            '*** *      * ''     ");
+            Console.WriteLine("*    ' *    ''  '''   ***   ''* **         **          '**                 *    ''*     ");
+            Console.WriteLine("*     *  ' *    ''  **                ''  **         **      ''    '*** *         *   * ");
+            Console.WriteLine("     ' *    ''  **          ''**           **     ''   '*** *       '    '***     *     ");
+            Console.WriteLine("  *  ' *    ''  *                *  *'                     *        *         *    ''*  ");
+            Console.WriteLine(" *         *              *    **                    *                            *     ");
+            Console.WriteLine("      *          * *                       *                                *           ");
+            Thread.Sleep(2000);
+            Console.Clear();
+
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("                           Welcome to the Wonder Woods");
+            Console.WriteLine("****************************************************************************************");
+            Thread.Sleep(2000);
+            Console.WriteLine("Your friend Kaj has been kidnapped by the evil Ice Queen...");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("The Ice Queen is planning to take over your home, the Wonder Woods!");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("You need to collect three keys and get to the Queen's Ice Palace to help him escape.");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("Expore the Wonder Woods and interact with the characters you meet...");
+            Console.WriteLine("****************************************************************************************");
+            Thread.Sleep(5000);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("But first, tell me your name...");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName + ", are you ready for an adventure?");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public void MainMenuMessage()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("                                 Main Menu");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("1) Go out and save your friend Kaj from the Ice Queen!");
+            Console.WriteLine("2) Show Stats");                        
+            Console.WriteLine("3) Show Rules of Game");
+            Console.WriteLine("4) Exit Game");
+            Console.WriteLine("****************************************************************************************");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public void RulesOfGame()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("                               RULES OF THE GAME");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("The game is divided into three levels. ");
+            Console.WriteLine("Each level will start with a Crossroad where you are asked to choose left or right.");
+            Console.WriteLine("If you chose to go the correct path through the Wonder Woods you will meet a character.");
+            Console.WriteLine("");
+            Console.WriteLine("The character will ask you a question that you must answer correctly to continue your journey.");
+            Console.WriteLine("The level will then continue with another Crossroad and so on, until you have correctly answered five questions.");
+            Console.WriteLine("You will collect a randomized amount of Diamonds during your quest and use them to buy yourself clues.");
+            Console.WriteLine("");
+            Console.WriteLine("When you have outwitted the fifth character on each level you will obtain one of the three keys.");
+            Console.WriteLine("The three keys you will use to set your friend Kaj free from the Evil Ice Queen.");
+            Console.WriteLine("If you are unable to correctly answer a question, you will be sent back one step in the game.");
+            Console.WriteLine("");
+            Console.WriteLine("GOOD LUCK!");
+            Thread.Sleep(10000); 
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public void MenuErrorMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Enter a number between 1-4 to chose an alternative from the menu.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void QuestionErrorMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("You answer the question by pressing 1, 2 or 3!");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
