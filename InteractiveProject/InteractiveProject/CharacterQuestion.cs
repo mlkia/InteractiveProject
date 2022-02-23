e0ddeedfc70bdd40f67f26494aa10f361b6baa73
Malik <mlk.2plus@gmail.com>
2022-02-23 10:43:09 +0100

    Fix in Character class

---
+++ InteractiveProject/InteractiveProject/CharacterQuestion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveProject
+{
+    internal class CharacterQuestion
+    {
+        public string Question { get; set; }
+        public string Alternative { get; set; }
+        public string Clue { get; set; }
+        public string CorrectAnswer { get; set; }
+
+
+        public static void CheckAnswer(Character character, CharacterQuestion characterQuestion, User user)
+        {
+            var userAnswer = Console.ReadLine();
+            if (userAnswer != characterQuestion.CorrectAnswer)
+            {
+                Console.WriteLine(character.IncorrectPhrase);
+                user.NumberOfWrongAnswers++;
+            }
+            else if (userAnswer == null || userAnswer != characterQuestion.Alternative)
+            {
+                Console.WriteLine("You're very funny, please answer from your options!");
+                user.NumberOfCorrectAnswers++;
+                user.Diamonds++;
+            }
+        }
+
+
+
+        //public void CheckAnswer(User user, QuestionHelper questionHelper)          // funkar inte för mig :(
+        //{
+        //    Console.Clear();
+        //    Console.WriteLine(questionHelper.Question);
+        //    Console.WriteLine(questionHelper.Alternatives);
+        //    Console.WriteLine("Answer by pressing a, b or c!");
+        //    string userAnswer = Console.ReadLine();
+
+        //    if (userAnswer == questionHelper.CorrectAnswer)
+        //    {
+        //        Console.WriteLine("Correct!");
+        //        RandomDiamond(user.Diamonds);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Wrong answer! You now go back to the last question...");
+        //    }
+    }
+}
+
--- InteractiveProject/InteractiveProject/User.cs
+++ InteractiveProject/InteractiveProject/User.cs
@@ -8,33 +8,34 @@ namespace InteractiveProject
 {
     internal class User
     {
-        public string UserName { get; set; } = "";
-        public int Diamonds { get; set; } = 0;
-        public int NumberOfCorrectAnswers { get; set; } = 0;
-        public int NumberOfWrongAnswers { get; set; } = 0;
+        public string UserName { get; set; }
+        public int Diamonds { get; set; }
+        public int NumberOfCorrectAnswers { get; set; }
+        public int NumberOfWrongAnswers { get; set; }
         public int Level { get; set; } = 1;
+        public int Key{ get; set; }
 
 
 
-    //    public void ShowStates()
-    //    {
-    //        var TheAnswers = new NumOfAnswer();
+        public void ShowStates()
+        {
+            var TheAnswers = new CharacterQuestion();
 
-    //        int[] numOfAnswers = new int[2];
+            //int[] numOfAnswers = new int[2];
 
-    //        numOfAnswers = TheAnswers.numOfAnswer();
+            //numOfAnswers = TheAnswers.numOfAnswer();
 
-    //        NumberOfCorrectAnswers = numOfAnswers[0];
-    //        NumberOfWrongAnswers = numOfAnswers[1];
+            //NumberOfCorrectAnswers = numOfAnswers[0];
+            //NumberOfWrongAnswers = numOfAnswers[1];
 
 
-    //        Console.WriteLine("Name:     " + UserName);
-    //        Console.WriteLine("You have: " + NumberOfCorrectAnswers + " Right answers");
-    //        Console.WriteLine("You have: " + NumberOfWrongAnswers + " Wrong answers");
-    //        Console.WriteLine("You have:    " + Diamonds + "Diamonds");
-    //        Console.WriteLine("You are in Level:      " + Level + "/3");
-    //    }
-    //}
+            Console.WriteLine("Name:     " + UserName);
+            Console.WriteLine("You have: " + NumberOfCorrectAnswers + " Right answers");
+            Console.WriteLine("You have: " + NumberOfWrongAnswers + " Wrong answers");
+            Console.WriteLine("You have:    " + Diamonds + "Diamonds");
+            Console.WriteLine("You are in Level:      " + Level + "/3");
+        }
+    }
 
 }
 }
