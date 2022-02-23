80f8ec1371c2cb33a5970e7289fba136c896ac58
Malik <mlk.2plus@gmail.com>
2022-02-23 15:02:13 +0100

    Update User and characterQuestion calsses

--- InteractiveProject/InteractiveProject/CharacterQuestion.cs
+++ InteractiveProject/InteractiveProject/CharacterQuestion.cs
@@ -14,20 +14,35 @@ namespace InteractiveProject
         public string CorrectAnswer { get; set; }
 
 
-        public static void CheckAnswer(Character character, CharacterQuestion characterQuestion, User user)
+        public int [] CheckAnswer()
         {
+            int[] values = new int[3];
+
+            var user = new User();
+            var character = new Character();
+            var characterQuestion = new CharacterQuestion();
+
             var userAnswer = Console.ReadLine();
             if (userAnswer != characterQuestion.CorrectAnswer)
             {
-                Console.WriteLine(character.IncorrectPhrase);
+                Console.WriteLine(character.IncorrectPhrase());
                 user.NumberOfWrongAnswers++;
+                values[1] = user.NumberOfWrongAnswers;
             }
             else if (userAnswer == null || userAnswer != characterQuestion.Alternative)
             {
                 Console.WriteLine("You're very funny, please answer from your options!");
+            }
+            else
+            {
+                Console.WriteLine(character.CorrectPhrase);
                 user.NumberOfCorrectAnswers++;
                 user.Diamonds++;
+                values[0] = user.NumberOfCorrectAnswers;
+                values[2] = user.Diamonds;
             }
+
+            return values;
         }
 
 
--- InteractiveProject/InteractiveProject/User.cs
+++ InteractiveProject/InteractiveProject/User.cs
@@ -8,34 +8,35 @@ namespace InteractiveProject
 {
     internal class User
     {
-        public string UserName { get; set; }
-        public int Diamonds { get; set; }
-        public int NumberOfCorrectAnswers { get; set; }
-        public int NumberOfWrongAnswers { get; set; }
+        public string UserName { get; set; } = "";
+        public int Diamonds { get; set; } = 0;
+        public int NumberOfCorrectAnswers { get; set; } = 0;
+        public int NumberOfWrongAnswers { get; set; } = 0;
         public int Level { get; set; } = 1;
-        public int Key{ get; set; }
+        public int Key { get; set; } = 0;
 
 
 
         public void ShowStates()
         {
-            var TheAnswers = new CharacterQuestion();
+            var Values = new CharacterQuestion();
 
-            //int[] numOfAnswers = new int[2];
 
-            //numOfAnswers = TheAnswers.numOfAnswer();
+
+            int[] TheValues = Values.CheckAnswer();
+
+            
 
             //NumberOfCorrectAnswers = numOfAnswers[0];
-            //NumberOfWrongAnswers = numOfAnswers[1];
+            //NumberOfWrongAnswers = TheValues[1];
+
 
 
             Console.WriteLine("Name:     " + UserName);
             Console.WriteLine("You have: " + NumberOfCorrectAnswers + " Right answers");
-            Console.WriteLine("You have: " + NumberOfWrongAnswers + " Wrong answers");
-            Console.WriteLine("You have:    " + Diamonds + "Diamonds");
+            Console.WriteLine("You have: " + TheValues[1]+ " Wrong answers");
+            Console.WriteLine("You have:    " + Diamonds + " Diamonds");
             Console.WriteLine("You are in Level:      " + Level + "/3");
         }
     }
-
-}
 }
