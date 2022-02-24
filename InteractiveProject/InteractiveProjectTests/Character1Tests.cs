02e8a0268ed4b69920221ee3cc5135ee55f5d311
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2022-02-24 14:45:26 +0100

    Added empty unit test project.

---
+++ InteractiveProject/InteractiveProjectTests/Character1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InteractiveProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveProject.Tests
+{
+    [TestClass()]
+    public class Character1Tests
+    {
+        [TestMethod()]
+        public void GenerateQuestionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
